795c42604adeaa764ff160fc25f71b3fffccf2b7
ajibdeji <ajibdeji@gmail.com>
2018-01-08 23:38:25 -0500

    final commit

--- app/src/main/java/com/example/bradj/eventit/Model/Adapter/OrganizationsAdapter.java
+++ app/src/main/java/com/example/bradj/eventit/Model/Adapter/OrganizationsAdapter.java
@@ -6,9 +6,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.bradj.eventit.Model.Entity.Organization;
+import com.example.bradj.eventit.Model.Entity.SubscribedOrg;
+import com.example.bradj.eventit.Model.Service.ApiUtils;
+import com.example.bradj.eventit.Model.Service.OrganizationService;
+import com.example.bradj.eventit.Model.Service.SubscribedOrgService;
 import com.example.bradj.eventit.R;
 
 import java.util.ArrayList;
@@ -25,6 +30,7 @@ public class OrganizationsAdapter extends RecyclerView.Adapter<OrganizationsAdap
     private List<Organization> dataList;
 
     private Activity activity;
+    private SubscribedOrgService subscribedOrgService= ApiUtils.getSubscribedOrgService();
 
     public OrganizationsAdapter(Context context, List<Organization> dataList) {
         this.context = context;
@@ -34,7 +40,6 @@ public class OrganizationsAdapter extends RecyclerView.Adapter<OrganizationsAdap
     public OrganizationsAdapter(Activity activity) {
         this.dataList = new ArrayList<>();
         this.activity = activity;
-
     }
     public void setDataList (List<Organization> list) {
         this.dataList=list;
@@ -49,9 +54,19 @@ public class OrganizationsAdapter extends RecyclerView.Adapter<OrganizationsAdap
     }
 
     @Override
-    public void onBindViewHolder(OrganizationsAdapter.MyViewHolder holder, int position) {
+    public void onBindViewHolder(OrganizationsAdapter.MyViewHolder holder, final int position) {
         holder.tvOrgDetail.setText(dataList.get(position).getName());
-
+//        holder.btnSubOrg.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                SubscribedOrg subscribedOrg=new SubscribedOrg();
+//                subscribedOrg.setOrg_id(Long.parseLong(""+dataList.get(position).getOrgId()));
+//                subscribedOrg.setUser_id(Long.parseLong(""+dataList.get(position).getOrgId()));
+//                subscribedOrg.setOrg_id(Long.parseLong(""+dataList.get(position).getOrgId()));
+//
+//                subscribedOrgService.addSubscribedOrg()
+//            }
+//        });
     }
 
     @Override
@@ -62,10 +77,12 @@ public class OrganizationsAdapter extends RecyclerView.Adapter<OrganizationsAdap
     public class MyViewHolder extends RecyclerView.ViewHolder{
 
         public TextView tvOrgDetail;
+        public Button btnSubOrg;
 
         public MyViewHolder(View itemView) {
             super(itemView);
             tvOrgDetail = (TextView) itemView.findViewById(R.id.tvOrgDetail);
+            btnSubOrg = (Button) itemView.findViewById(R.id.btnSubscribeOrg);
         }
     }
 
